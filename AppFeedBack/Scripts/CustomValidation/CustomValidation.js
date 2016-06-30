9630cf4a3cb72443b8bc86aab0fccb743cb7a4da
Andrey1661 <shedogubov.andrey@gmail.com>
2016-06-30 09:42:06 +0700

    Day 3

---
+++ AppFeedBack/Scripts/CustomValidation/CustomValidation.js
@@ -0,0 +1,10 @@
+﻿
+$.validator.addMethod("notempty", function(value, element, params) {
+    return value !== $(params).val();
+});
+
+
+$.validator.unobtrusive.adapters.add("notempty", ["empty"], function (options) {
+    options.rules["notempty"] = "#" + options.params.empty;
+    options.messages["notempty"] = options.messages;
+});
--- AppFeedBack/Validation/NotEmptyAttribute.cs
+++ AppFeedBack/Validation/NotEmptyAttribute.cs
@@ -3,23 +3,40 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace AppFeedBack.Validation
 {
     /// <summary>
     /// Атрибут, не допускающий объект типа Guid равным значению Guid.Empty
     /// </summary>
-    public class NotEmptyAttribute : ValidationAttribute
+    public class NotEmptyAttribute : ValidationAttribute, IClientValidatable
     {
+        private readonly Guid _empty = Guid.Empty;
+
         public override bool IsValid(object value)
         {
             var guid = (Guid) value;
-            if (guid != Guid.Empty)
+            if (guid != _empty)
             {
                 return true;
             }
 
             return false;
+        }
+
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata,
+            ControllerContext context)
+        {
+            ModelClientValidationRule notNullRule = new ModelClientValidationRule
+            {
+                ErrorMessage = ErrorMessageString,
+                ValidationType = "notempty"
+            };
+
+            notNullRule.ValidationParameters.Add("empty", _empty);
+
+            yield return notNullRule;
         }
     }
 }
