8ea9215915a09b3942d45b6074053368b613de1f
Andrey1661 <shedogubov.andrey@gmail.com>
2016-07-05 14:33:02 +0700

    .gitignore added

---
+++ AppFeedBack.Domain/Entities/FeedbackFile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppFeedBack.Domain.Entities
+{
+    /// <summary>
+    /// Модель прикрепленного к отзыву файла
+    /// </summary>
+    public class FeedbackFile
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Физический путь к файлу, хранящемуся на сервере
+        /// </summary>
+        [Required]
+        public string FilePath { get; set; }
+
+        [ForeignKey("Feedback")]
+        public Guid FeedbackId { get; set; }
+
+        /// <summary>
+        /// Ссылка на родительский отзыв
+        /// </summary>
+        public virtual Feedback Feedback { get; set; }
+
+        public FeedbackFile() { }
+
+        public FeedbackFile(string path)
+        {
+            FilePath = path;
+        }
+    }
+}
